972d63bae31389fa86c0e2d5fa0b1bc46888251e
hmzhan <hmzhan@gmail.com>
2024-10-26 17:36:30 -0400

    added student ckpt and teacher ckpt in constants.py

--- src/efficient_llm/constants.py
+++ src/efficient_llm/constants.py
@@ -1,2 +1,8 @@
+import torch
 
 MODEL_CKPT = "transformersbook/bert-base-uncased-finetuned-clinc"
+STUDENT_CKPT = "distillbert-base-uncased"
+TEACHER_CKPT = "transformersbook/bert-base-uncased-finetuned-clinc"
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
