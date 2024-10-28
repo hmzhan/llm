f6b74698e18b38bd0ddee8634b04ca8a52cba916
hmzhan <hmzhan@gmail.com>
2024-10-28 12:00:16 -0400

    refactored code for distillation trainer

--- src/efficient_llm/knowledge_distillation.py
+++ src/efficient_llm/knowledge_distillation.py
@@ -1,10 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import Trainer
-from transformers import TrainingArguments
+from transformers import (
+    Trainer,
+    TrainingArguments,
+    AutoConfig,
+    AutoTokenizer,
+    AutoModelForSequenceClassification
+)
 from datasets import load_metric
 import numpy as np
+from src.efficient_llm.constants import STUDENT_CKPT, TEACHER_CKPT, DEVICE
+from src.efficient_llm.data import clinc
 
 accuracy_score = load_metric("accuracy", trust_remote_code=True)
 
@@ -48,3 +55,53 @@ def compute_metrics(pred):
     predictions, labels = pred
     predictions = np.argmax(predictions, axis=1)
     return accuracy_score.compute(predictions=predictions, references=labels)
+
+
+def student_init(pipe):
+    student_config = AutoConfig.from_pretrained(
+        STUDENT_CKPT,
+        num_labels=clinc["test"].features["intent"].num_classes,
+        id2label=pipe.model.config.id2label,
+        label2id=pipe.model.config.label2id
+    )
+    return AutoModelForSequenceClassification.from_pretrained(STUDENT_CKPT, config=student_config).to(DEVICE)
+
+
+def tokenize_text(batch):
+    student_tokenizer = AutoTokenizer.from_pretrained(STUDENT_CKPT)
+    return student_tokenizer(batch["text"], truncation=True)
+
+
+def knowledge_distillation():
+    # student toknizer
+    student_tokenizer = AutoTokenizer.from_pretrained(STUDENT_CKPT)
+    # teacher model
+    teacher_model = AutoModelForSequenceClassification.from_pretrained(
+        TEACHER_CKPT, num_labels=clinc["test"].features["intent"].num_classes).to(DEVICE)
+    # dataset
+    clinc_enc = clinc.map(tokenize_text, batched=True, remove_columns=["text"])
+    clinc_enc = clinc_enc.rename_column("intent", "labels")
+    # training args
+    training_args = DistillationTrainingArguments(
+        output_dir="distillbert-base-uncased-finetuned-clinc",
+        evaluation_strategy="epoch",
+        num_train_epochs=10,
+        temperature=7,
+        learning_rate=2e-5,
+        per_device_train_batch_size=48,
+        per_device_eval_batch_size=48,
+        alpha=0.12,
+        weight_decay=0.01,
+        push_to_hub=True,
+        report_to="none"
+    )
+    # Trainer
+    return DistillationTrainer(
+        model_init=student_init,
+        teacher_model=teacher_model,
+        args=training_args,
+        train_dataset=clinc_enc['train'],
+        eval_dataset=clinc_enc['validation'],
+        compute_metrics=compute_metrics,
+        tokenizer=student_tokenizer
+    )
