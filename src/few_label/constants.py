47ffa078617fa289768f2544db332febf609de5f
hmzhan <hmzhan@gmail.com>
2024-10-29 16:42:12 -0400

    added constants.py and data.py

---
+++ src/few_label/constants.py
@@ -0,0 +1,14 @@
+
+DATASET_URL = "https://git.io/nlp-with-transformers"
+
+LABEL_MAP = {
+    'Core: Tokenization': 'tokenization',
+    'New model': 'new model',
+    'Core: Modeling': 'model training',
+    'Usage': 'usage',
+    'Core: Pipeline': 'pipeline',
+    'Tensorflow': 'tensorflow or tf',
+    'PyTorch': 'pytorch',
+    'Examples': 'examples',
+    'Documentation': 'documentation'
+}
