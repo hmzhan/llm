1aa1908e2ae083ba56b450e813f6278301454538
hmzhan <hmzhan@gmail.com>
2024-11-07 14:05:52 -0500

    added constants.py and gemma.py in gemma folder

---
+++ src/fine_tuning/gemma/constants.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    output_dir: str = "output"
+    model_ckpt: str = "unsloth/gemma-2-9b-it-bnb-4bit"  # 4-bit quantized gemma-2-9b-instruct
+    max_length: int = 1024
+    n_splits: int = 5
+    fold_idx: int = 0
+    optim_type: str = "adamw_8bit"
+    per_device_train_batch_size: int = 2
+    gradient_accumulation_steps: int = 2  # global batch size is 8
+    per_device_eval_batch_size: int = 8
+    n_epochs: int = 1
+    freeze_layers: int = 16  # there're 42 layers in total, we don't add adapters to the first 16 layers
+    lr: float = 2e-4
+    warmup_steps: int = 20
+    lora_r: int = 16
+    lora_alpha: float = lora_r * 2
+    lora_dropout: float = 0.05
+    lora_bias: str = "none"
